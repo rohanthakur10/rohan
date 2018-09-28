26eec73b1cb4bcd3f643df2cd52637d2156439a3
rohanthakur10 <rajput.rohan.thakur@gmail.com>
2018-09-28 15:11:34 +0530

    first commit

---
+++ assignment.py
@@ -0,0 +1,26 @@
+#Q.1- Print anything you want on screen.
+print('I am Rohan Thakur B.Tech (c.s.e) 7TH sem')
+#Q.2- Join two strings using '+'.
+r="rohan"+"thakur"
+#Q.3- Take the input of 3 variables x, y and z . Print their values on screen.
+x=input('enter the value of x')
+y=input('enter the value of y')
+z=input('enter the value of z')
+#Q.4- Print “Let’s get started” on screen.
+print("Let's get started")
+#Q.5- Print the following values using placeholders.
+#s=”Acadview”
+#course=”Python”
+#fees=5000
+s="Acadview"
+course="Python"
+fees=5000
+print("%s %s %d"%(s,course,fees))
+#Q.6- Find the area of circle
+#pi = 3.14
+#Take radius as input from user
+#Print the area of circle 
+pi=3.14
+r=int(input("enter the value of radius"))
+area=pi*r*r
+print("area of circle",area)
